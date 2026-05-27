0a0df17a0ae4192c86d240568839ba20a52a7642
INEPAKALE <pwwiw61@gmail.com>
2026-05-27 15:07:59 +0800

    feat: complete CLI migration with --remove flag and argparse integration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
+import os, argparse
 
 DB_FILE = "blacklist.txt"
+
 
 def save_data(data_list, filename):
         with open(filename, "w", encoding='utf-8') as file:
@@ -13,33 +14,32 @@ else:
     blacklist = set()
     print("Database file not found, new list created.")
 
-while True:
-    name = input('Enter your name (or "off" to exit): ').capitalize().strip()
-    
-    if name == "Off":
-        break
+parser = argparse.ArgumentParser(description="CLI blacklist-system")
+#Add username to list
+parser.add_argument("--add", type=str, help="Add username to list")
+#Checking a Name in a List
+parser.add_argument("--check", type=str, help="Check for the presence of a name")
+#Removing a user from the list
+parser.add_argument("--remove", type=str, help="Removing a user from the list")
+args = parser.parse_args()
 
-    elif name == "Add":
-        new_name = input('Enter a name for the blacklist: ').capitalize().strip()
-        blacklist.add(new_name)
-      
-        save_data(blacklist, DB_FILE)
-        print(f'{new_name} added and saved.')
-        continue
-
-    elif name == "Del":
-        rm_name = input('Who to remove: ').capitalize().strip()
-        if rm_name in blacklist:
-            blacklist.remove(rm_name)
-
-            save_data(blacklist, DB_FILE)
-            print(f'{rm_name} removed from the list and file.')
-        else:
-            print('This name is not on the list.')
-        continue
-
-
-    if name in blacklist:
+if args.add:
+    new_name = args.add.capitalize().strip()
+    blacklist.add(new_name)
+    save_data(blacklist, DB_FILE)
+    print(f'{new_name} added and saved.')
+elif args.check:
+    name_to_check = args.check.capitalize().strip()
+    print(blacklist)
+    if name_to_check in blacklist:
         print('ACCESS DENIED!')
     else:
         print('Welcome, access granted.')
+elif args.remove:
+    rm_name = args.remove.capitalize().strip()
+    if rm_name in blacklist:
+        blacklist.remove(rm_name)
+        save_data(blacklist, DB_FILE)
+        print(f'{rm_name} removed from the list and file.')
+    else:
+        print('This name is not on the list.')
